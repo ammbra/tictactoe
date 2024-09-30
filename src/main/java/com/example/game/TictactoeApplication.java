3f9388ca4ab7f7f80e86a3c81c29f8414fedb1fe
ammbra <mihalceanuana@gmail.com>
2024-09-30 12:08:38 +0200

    Remove unused imports.

--- src/main/java/com/example/game/TictactoeApplication.java
+++ src/main/java/com/example/game/TictactoeApplication.java
@@ -3,14 +3,10 @@ package com.example.game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.lang.reflect.Field;
-import java.security.Security;
-import java.util.Properties;
-
 @SpringBootApplication
 public class TictactoeApplication {
 
-	public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+	public static void main(String[] args) {
 		SpringApplication.run(TictactoeApplication.class, args);
 	}
 
